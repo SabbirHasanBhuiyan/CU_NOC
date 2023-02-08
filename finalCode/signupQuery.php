e6cf0ff50ee92f5248e5f882586599be52ba8037
SabbirHasanBhuiyan <sabbirhasan675@gmail.com>
2023-02-08 23:11:23 +0600

    signupQuery page done

--- finalCode/signupQuery.php
+++ finalCode/signupQuery.php
@@ -1,22 +1,46 @@
 <?php
-
-$errors= array('email'=>'','password'=>'');
-if($_SERVER["REQUEST_METHOD"]=="POST"){
     include 'db_connect.php';
-    $username = $_POST["first_name"] . " " . $_POST["last_name"];
-    $email = $_POST["user_email"];
-    $pass = $_POST["user_password"];
-    $department = $_POST["dept"];
-    $confirmPass=$_POST['repeat_password'];
+    $errors= array('email'=>'','password'=>'');
+    $email=$pass=$department=$confirmPass=$fName=$lName=$department='';
 
+    if($_SERVER["REQUEST_METHOD"]=="POST"){
+        $email = $_POST["user_email"];
+        $pass = $_POST["user_password"];
+        $department = $_POST["dept"];
+        $confirmPass=$_POST['repeat_password'];
+        $fName=$_POST['first_name'];
+        $lName=$_POST['last_name'];
+
+        if(!empty($email)){
+            $sql="SELECT * FROM user where email='$email'";
+            $result=mysqli_query($connection,$sql);
+            $user=mysqli_fetch_assoc($result);;
+            mysqli_free_result($result);            
     
+            if(!empty($user)){
+                $errors['email']='This email has an account already';
+            }
+        }
 
+        if($pass!=$confirmPass){
+            $errors['password']='Password does not matches';
+        }
 
-    $sqlQuery="INSERT INTO `user` ( `Name`, `Email`, `Password`, `Department`) VALUES ('$username', '$email', '$pass', '$department')";
+        
+        if(!array_filter($errors)){
+            $email=mysqli_real_escape_string($connection,$_POST['user_email']);
+            $password=mysqli_real_escape_string($connection,$_POST['user_password']);
+            $username = mysqli_real_escape_string($connection,$_POST['first_name']) . " " . mysqli_real_escape_string($connection,$_POST['last_name']);
+            $sqlQuery="INSERT INTO user( `Name`, `Email`, `Password`, `Department`) VALUES ('$username', '$email', '$pass', '$department')";
+            
+            if(mysqli_query($connection,$sqlQuery)){
+                header('Location: index.php');
+            }else{
+                echo 'query error : '.mysqli_error($connection);
+            }
+        }
 
-    $result= mysqli_query($connection,$sqlQuery);
-
-}
+    }
 
 
 ?>
