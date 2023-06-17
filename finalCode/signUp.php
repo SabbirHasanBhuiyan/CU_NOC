8ff21439867beb776e668a43dc0fc71d1e11ba04
souravkcoder <souravk.coder@gmail.com>
2023-06-17 19:10:49 +0600

    signup page aligned center

--- finalCode/signUp.php
+++ finalCode/signUp.php
@@ -30,7 +30,7 @@
                       Form
          -------------------------------->
          <div>
-    <form class=" mt-5 p-5 ms-20 mx-auto h-4/5 w-3/5 md:w-2/5 bg-base-100 bg-blue-100 drop-shadow-2xl  border-solid border-2  border-indigo-400 rounded-lg" action="\CU_NOC\finalCode\signUp.php" method="POST">
+    <form class=" mt-5 p-5 ms-3/5 mx-auto h-4/5 w-3/5 md:w-2/5 bg-base-100 bg-blue-100 drop-shadow-2xl  border-solid border-2  border-indigo-400 rounded-lg" action="\CU_NOC\finalCode\signUp.php" method="POST">
       <div class="relative z-0 w-full mb-6 group mt-5">
           <input type="email" name="user_email" id="user_email" class="block py-2.5 px-0 w-full text-sm text-black-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder=" " required />
           <label for="floating_email" class="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Email address</label>
@@ -62,7 +62,7 @@
     </select>
    </div>
       <div class="">
-        <button type="submit" class="mx-30 mb-20 mt-5 text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Submit</button>
+        <button type="submit" class="ms-3/5 mb-20 mt-5 text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Submit</button>
       </div>
     </form>
   </div>
